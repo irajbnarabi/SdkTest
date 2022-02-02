57ff3a55633e36790a265945d32d7fc6f87ea033
Mahdiar <mahjoubi.mahdiar@gmail.com>
2022-02-02 17:02:48 +0330

    --

---
+++ EmptyLibrary/src/main/java/com/mahdiar/emptylibrary/ShowEmptyMessage.java
@@ -0,0 +1,12 @@
+package com.mahdiar.emptylibrary;
+
+import android.util.Log;
+
+public class ShowEmptyMessage {
+
+    static void LogformLib(){
+        Log.e("EmptyLib" , "this is for Empty Library");
+
+    }
+
+}
